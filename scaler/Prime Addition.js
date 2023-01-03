a1757553127ce0bf0c7820f72e0d3b1fd3fa6f79
Rohant <agrawalrohant@gmail.com>
2023-01-03 17:08:13 +0530

    Create Prime Addition.js

---
+++ scaler/Prime Addition.js
@@ -0,0 +1,13 @@
+// question --> https://www.scaler.com/academy/mentee-dashboard/class/41053/homework/problems/1262?navref=cl_tt_nv
+
+module.exports = {
+  //param A : integer
+  //return an integer
+  solve: function (A) {
+    if (A == 2) {
+      return 1;
+    } else {
+      return 2;
+    }
+  },
+};
