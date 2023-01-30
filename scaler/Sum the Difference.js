1c3e87c974b53ae5dd58a81e329b4cfdb7d7d4db
Rohant <agrawalrohant@gmail.com>
2023-01-30 11:16:19 +0530

    Create Sum the Difference.js

---
+++ scaler/Sum the Difference.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //param A : array of integers
+  //return an integer
+  solve: function (A) {
+    let ans = 0;
+    let mod = 1000000007;
+    let max = 0;
+    let min = 0;
+    let N = A.length;
+    A.sort((a, b) => a - b);
+    for (let i = 0; i < N; i++) {
+      min *= 2;
+      min = (min + A[i]) % mod;
+      let j = N - i - 1;
+      max *= 2;
+      max = (max + A[j]) % mod;
+    }
+    return (max - min + mod) % mod;
+  },
+};
