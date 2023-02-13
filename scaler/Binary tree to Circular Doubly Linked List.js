eb1f79a154dc827bd4e301db9544d147eedb45e7
Rohant <agrawalrohant@gmail.com>
2023-02-13 20:29:34 +0530

    Update Binary tree to Circular Doubly Linked List.js

--- scaler/Binary tree to Circular Doubly Linked List.js
+++ scaler/Binary tree to Circular Doubly Linked List.js
@@ -4,29 +4,29 @@
 //return the root node in the tree
 function solve(A) {
   return convertToDLL(A);
+}
 
-  function convertToDLL(root) {
-    if (root == null) {
-      return [null, null];
-    }
-    let [first, last] = convertToDLL(root.left);
-    if (last != null) {
-      last.right = root;
-      root.left = last;
-    }
-    let [first2, last2] = convertToDLL(root.right);
-    if (first2 != null) {
-      root.right = first2;
-      first2.left = root;
-    }
-    if (first == null && last2 == null) {
-      return [root, root];
-    } else if (first == null) {
-      return [root, last2];
-    } else if (last2 == null) {
-      return [first, root];
-    } else {
-      return [first, last2];
-    }
+function convertToDLL(root) {
+  if (root == null) {
+    return null;
   }
+  let leftDLL = convertToDLL(root.left);
+  let rightDLL = convertToDLL(root.right);
+  root.left = root.right = root;
+  let leftWithRoot = connect(leftDLL, root);
+  return connect(leftWithRoot, rightDLL);
+}
+
+function connect(leftList, rightList) {
+  if (leftList == null) return rightList;
+  if (rightList == null) return leftList;
+
+  let leftLast = leftList.left;
+
+  let rightLast = rightList.left;
+  if (leftLast) leftLast.right = rightList;
+  rightList.left = leftLast;
+  leftList.left = rightLast;
+  if (rightLast) rightLast.right = leftList;
+  return leftList;
 }
