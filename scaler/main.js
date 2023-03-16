19d8b5170bf616673033a15c1a1a703de5bedce5
Rohant <agrawalrohant@gmail.com>
2023-03-16 23:13:01 +0530

    Update main.js

--- scaler/main.js
+++ scaler/main.js
@@ -1,14 +1,16 @@
-var filename = "./Unique Elements.js"; // Update the file name to debug here
+var filename = "./Rotten Oranges.js"; // Update the file name to debug here
 
 var scalerModule = require(filename);
 
-// Update the paramters to pass
-var reuslt = scalerModule.solve([
-  51, 6, 10, 8, 22, 61, 56, 48, 88, 85, 21, 98, 81, 76, 71, 68, 18, 6, 14, 23,
-  72, 18, 56, 30, 97, 100, 81, 5, 99, 2, 85, 67, 46, 32, 66, 51, 76, 53, 36, 31,
-  81, 56, 26, 75, 69, 54, 54, 54, 83, 41, 86, 48, 7, 32, 85, 23, 47, 23, 18, 45,
-  79, 95, 73, 15, 55, 16, 66, 73, 13, 85, 14, 80, 39, 92, 66, 20, 22, 25, 34,
-  14, 51, 14, 17, 10, 100, 35, 9, 83, 31, 60, 24, 37, 69, 62,
+let reuslt = scalerModule.solve([
+  [2, 0, 2, 2, 2, 0, 2, 1, 1, 0],
+  [0, 1, 2, 0, 2, 0, 0, 1, 0, 1],
+  [0, 1, 1, 1, 2, 0, 1, 1, 2, 1],
+  [2, 0, 2, 0, 1, 1, 2, 1, 0, 1],
+  [1, 0, 1, 1, 0, 1, 2, 0, 2, 2],
+  [0, 2, 1, 1, 2, 2, 0, 2, 1, 2],
+  [2, 1, 0, 2, 0, 0, 0, 0, 1, 1],
+  [2, 2, 0, 2, 2, 1, 1, 1, 2, 2],
 ]);
 
 console.log("Result : " + reuslt);
