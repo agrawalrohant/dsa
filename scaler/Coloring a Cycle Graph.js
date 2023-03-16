4a6cd562b0e10022a6adea6505e73e0ecf9e5ff6
Rohant <agrawalrohant@gmail.com>
2023-03-16 23:21:53 +0530

    Create Coloring a Cycle Graph.js

---
+++ scaler/Coloring a Cycle Graph.js
@@ -0,0 +1,14 @@
+// question -->https://www.scaler.com/academy/mentee-dashboard/class/70585/assignment/problems/9358/?navref=cl_pb_nv_tb
+
+module.exports = { 
+    //param A : integer
+    //return an integer
+       solve : function(A){
+           if(A % 2 ==0){
+               return 2;
+           }else{
+               return 3;
+           }
+       }
+   };
+   
