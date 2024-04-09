fff84c1b122491904d1641862d37fa909827e1a1
Rohant <agrawalrohant@gmail.com>
2024-04-09 23:48:21 +0530

    Update Edit Distance.js

--- scaler/Edit Distance.js
+++ scaler/Edit Distance.js
@@ -1,6 +1,46 @@
 // question -->https://www.scaler.com/academy/mentee-dashboard/class/52322/assignment/problems/21?navref=cl_tt_lst_nm
 
+// Recurssive Approch
 module.exports = {
+  //param A : string
+  //param B : string
+  //return an integer
+  minDistance: function (A, B) {
+    let dp = [];
+    for (let i = 0; i < A.length; i++) {
+      dp.push([]);
+      for (let j = 0; j < B.length; j++) {
+        dp[i].push(-1);
+      }
+    }
+    return this.getEditDist(A, A.length - 1, B, B.length - 1, dp);
+  },
+  getEditDist: function (A, i, B, j, dp) {
+    if (i < 0 && j < 0) {
+      return 0;
+    }
+    if (i < 0) {
+      return j + 1;
+    }
+    if (j < 0) {
+      return i + 1;
+    }
+    if (dp[i][j] == -1) {
+      if (A[i] == B[j]) {
+        dp[i][j] = this.getEditDist(A, i - 1, B, j - 1, dp);
+      } else {
+        let onInsert = 1 + this.getEditDist(A, i, B, j - 1, dp);
+        let onDelete = 1 + this.getEditDist(A, i - 1, B, j, dp);
+        let onReplace = 1 + this.getEditDist(A, i - 1, B, j - 1, dp);
+        dp[i][j] = Math.min(onInsert, onDelete, onReplace);
+      }
+    }
+    return dp[i][j];
+  },
+};
+
+// Iterative Approch
+/*module.exports = {
   //param A : string
   //param B : string
   //return an integer
@@ -45,4 +85,4 @@ module.exports = {
       return DP[a][b];
     }
   },
-};
+};*/
